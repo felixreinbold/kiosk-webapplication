3c18ce9456b656b838eb2248d8a4d004f7e204df
felixreinbold <felix.reinbold2003@gmail.com>
2025-07-31 09:11:25 +0200

    Initial commit für Spring Boot Backend

---
+++ kiosk-backend/src/main/java/com/kiosk/kioskbackend/KioskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kiosk.kioskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskBackendApplication.class, args);
+    }
+
+}
